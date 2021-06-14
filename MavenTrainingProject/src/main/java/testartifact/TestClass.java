f363448097cf5a2901931d92b2770984c4f45353
sthamim <sheik.thamim.ansari@gmail.com>
2021-06-14 22:11:04 +0530

    FIrst commit

---
+++ MavenTrainingProject/src/main/java/testartifact/TestClass.java
@@ -0,0 +1,10 @@
+package testartifact;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am doing training on Jenkins");
+	}
+
+}
