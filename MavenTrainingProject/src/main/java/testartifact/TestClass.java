2c05c680b040051481afabeab74ebd88943ef623
sthamim <sheik.thamim.ansari@gmail.com>
2021-06-17 00:46:38 +0530

    upated testclass

--- MavenTrainingProject/src/main/java/testartifact/TestClass.java
+++ MavenTrainingProject/src/main/java/testartifact/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I am doing training on Jenkins....");
+		System.out.println("I am doing training on Jenkins......");
 	}
 
 }
