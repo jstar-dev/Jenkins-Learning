2164e673bfa328b936821a58acb3341d30096aac
jstar-dev <sheik.thamim.ansari@gmail.com>
2021-07-03 00:45:20 +0530

    Update gitcheckoutnormal.groovy

--- src/com/training/globalsharedlib/gitcheckoutnormal.groovy
+++ src/com/training/globalsharedlib/gitcheckoutnormal.groovy
@@ -1,5 +1,5 @@
 package com.training.globalsharedlib
 def call(repo,branch,credentialsId) {
 	// git "changelog: false, credentialsId: ${credentialsId}, poll: false, url: ${repo}, branch: ${branch}"
-	 git changelog: false, credentialsId: 'github-credentials', poll: false, url: ${url}, branch: 'master'
+	 git changelog: false, credentialsId: 'github-credentials', poll: false, url: ${repo}, branch: 'master'
 }
