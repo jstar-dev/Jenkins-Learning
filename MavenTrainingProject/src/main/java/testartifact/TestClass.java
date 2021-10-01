c19afea2f27644a6551e9e6832426591085c3f3e
jstar-dev <sheik.thamim.ansari@gmail.com>
2021-10-01 19:22:45 +0530

    Update TestClass.java

--- MavenTrainingProject/src/main/java/testartifact/TestClass.java
+++ MavenTrainingProject/src/main/java/testartifact/TestClass.java
@@ -6,6 +6,7 @@ public class TestClass {
 		// TODO Auto-generated method stub
 		System.out.println("I am doing training on Jenkins........");
 		System.out.println("Tetsing Poling functionality...");
+		System.out.println("Tetsing Polling functionality one more time...");
 	}
 
 }
